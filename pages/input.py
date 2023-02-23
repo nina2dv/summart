041de257b13882f628e85b27cabf5aa38aafb8e1
nina2dv <112736309+nina2dv@users.noreply.github.com>
2023-02-23 01:50:24 -0500

    Update input.py

--- pages/input.py
+++ pages/input.py
@@ -3,10 +3,23 @@ import openai
 
 openai.api_key = st.secrets['openai_KEY']
 
+# prompt="Please find the key insights from the below text in maximum of 5 bullet points list format and also the summary in maximum of 3 sentences:\n" + text,
+
 def extract_key_findings(text):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Please find the key insights from the below text in maximum of 5 bullet points list format and also the summary in maximum of 3 sentences:\n" + text,
+        prompt="Please find the key insights from the below text in maximum of 5 bullet points list format:\n" + text,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0)
+    return response.choices[0].text
+
+def extract_summary(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Please create a summary from the below text  in maximum of 3 sentences:\n" + text,
         temperature=0.7,
         max_tokens=256,
         top_p=1,
@@ -57,14 +70,23 @@ if input_text is not None:
     if st.button("Analyze Text"):
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown("**Key Findings based on your Text**")
+            st.markdown("**Key Findings**")
             st.success(extract_key_findings(input_text))
+            
+            st.markdown("""---""")
+
+            st.markdown("**Summary**")
+            st.success(extract_summary(input_text))
         with col2:
             st.markdown("**Keywords**")
             st.info(key_words(input_text))
+            
             st.markdown("""---""")
+            
             st.markdown("**Most Positive Words**")
             st.success(most_positive_words(input_text))
+            
             st.markdown("""---""")
+            
             st.markdown("**Most Negative Words**")
             st.error(most_negative_words(input_text))
