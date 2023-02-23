4bf71e9e1027dd2b4d1f7ac10b70e9f0a9d77365
nina2dv <112736309+nina2dv@users.noreply.github.com>
2023-02-23 01:38:49 -0500

    Update input.py

--- pages/input.py
+++ pages/input.py
@@ -6,7 +6,7 @@ openai.api_key = st.secrets['openai_KEY']
 def extract_key_findings(text):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Please find the key insights from the below text in maximum of 5 bullet points and also the summary in maximum of 3 sentences:\n" + text,
+        prompt="Please find the key insights from the below text in maximum of 5 bullet points list format and also the summary in maximum of 3 sentences:\n" + text,
         temperature=0.7,
         max_tokens=256,
         top_p=1,
@@ -14,6 +14,16 @@ def extract_key_findings(text):
         presence_penalty=0)
     return response.choices[0].text
 
+def key_words(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Please extract the important keywords from the below text\n" + text,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0)
+    return response.choices[0].text
 
 def most_positive_words(text):
     response = openai.Completion.create(
@@ -36,9 +46,10 @@ def most_negative_words(text):
         frequency_penalty=0,
         presence_penalty=0)
     return response.choices[0].text
+
 # Main Page
 
-st.title("Summarize Input :page_with_curl:")
+st.title("Input Analyzer :page_with_curl:")
 
 with st.expander("About this application"):
     st.markdown("This app is built using the [OpenAI GPT3](https://platform.openai.com/) and Streamlit")
@@ -52,6 +63,9 @@ if input_text is not None:
             st.markdown("**Key Findings based on your Text**")
             st.success(extract_key_findings(input_text))
         with col2:
+            st.markdown("**Keywords**")
+            st.info(key_words(input_text))
+            st.markdown("""---""")
             st.markdown("**Most Positive Words**")
             st.success(most_positive_words(input_text))
             st.markdown("""---""")
